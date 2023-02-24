daa648c84d234da680f14cd0a51e6d3ea0cfb809
RamatoulayeTraore <125762431+RamatoulayeTraore@users.noreply.github.com>
2023-02-24 16:41:47 +0100

    modification du script

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,8 +1,14 @@
-from graph import Graph, graph_from_file
+from graph import Graph,graph_from_file
 
 
 data_path = "input/"
 file_name = "network.01.in"
 
-g = graph_from_file(data_path + file_name)
+g=graph_from_file("..\input\network.00.in")
 print(g)
+
+
+"""g=Graph([])
+g.add_edge("Paris", "Palaiseau", 4, 20)
+print(g)
+"""
