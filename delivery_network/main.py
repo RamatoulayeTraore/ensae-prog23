2f0231604e8bd8373b839849ae3a280f44c61bcb
RamatoulayeTraore <125762431+RamatoulayeTraore@users.noreply.github.com>
2023-03-15 18:06:41 +0100

    mise a jour 2

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,17 +1,32 @@
+import time
 from graph import Graph,graph_from_file
 
 
 data_path = "input/"
 file_name = "network.01.in"
 
-g=graph_from_file("network.00.in")
+""" g=graph_from_file("input/network.5.in")
+print(g) """
+g = graph_from_file("input/routes.1.in")
 print(g)
+####test_route_7
+""" t0 = time.perf_counter()
+g.min_power(4,12)
+g.min_power(3,15)
+g.min_power(15,9)
+t1=time.perf_counter()
+tmps_moy=(t1-t0)/3 
+print("temps moy",tmps_moy)  
+####test_route_7
+g1 = graph_from_file("input/routes.7.in")
+t0 = time.perf_counter()
+g1.min_power(194708,87133)
+t1=time.perf_counter()
+tmps_moy2=(t1-t0)
+print("temps moy2",tmps_moy2)
+ """
 
-
-"""g=Graph([])
-g.add_edge("Paris", "Palaiseau", 4)
-print(g)
-"""
-
-
-
+ 
+""" g=Graph([])
+    g.add_edge("Paris", "Palaiseau", 4, 20)
+    print(g) """
