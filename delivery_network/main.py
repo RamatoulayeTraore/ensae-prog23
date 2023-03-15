48b01d266895d0540dc46949df558658d4a4b0c1
RamatoulayeTraore <125762431+RamatoulayeTraore@users.noreply.github.com>
2023-03-15 17:57:44 +0100

    complexité en cours

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -5,24 +5,26 @@ from graph import Graph,graph_from_file
 data_path = "input/"
 file_name = "network.01.in"
 
-g=graph_from_file("input/network.5.in")
+""" g=graph_from_file("input/network.5.in")
+print(g) """
+g = graph_from_file("input/routes.1.in")
 print(g)
-""" g = graph_from_file("input/routes.1.in")
-t0 = time.perf_counter()
+####test_route_7
+""" t0 = time.perf_counter()
 g.min_power(4,12)
 g.min_power(3,15)
 g.min_power(15,9)
 t1=time.perf_counter()
 tmps_moy=(t1-t0)/3 
-print("temps moy",tmps_moy)  """
-""" ####test_route_10
+print("temps moy",tmps_moy)  
+####test_route_7
 g1 = graph_from_file("input/routes.7.in")
 t0 = time.perf_counter()
 g1.min_power(194708,87133)
 t1=time.perf_counter()
-tmps_moy=(t1-t0)
-print("temps moy",tmps_moy) """
-
+tmps_moy2=(t1-t0)
+print("temps moy2",tmps_moy2)
+ """
 
  
 """ g=Graph([])
