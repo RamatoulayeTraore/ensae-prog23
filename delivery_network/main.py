09ad0ec8cb323e17bbcb73473cced4bcaffa6417
RamatoulayeTraore <125762431+RamatoulayeTraore@users.noreply.github.com>
2023-04-03 12:54:07 +0200

    hjkdk

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,6 +1,6 @@
 import time
-from graph import Graph,graph_from_file,UF,kruskal
-from essai import create_output_file
+from graph import Graph,graph_from_file,
+from essai import create_output_file,kruskal
 
 
 
@@ -9,7 +9,8 @@ file_name = "network.01.in"
 
 """ g=graph_from_file("input/network.5.in")
 print(g) """
-#g = graph_from_file("input/network.2.in")
+g = graph_from_file("input/network.00.in")
+print(kruskal(g))
 ####test_route_7
 """ t0 = time.perf_counter()
 g.min_power(4,12)
@@ -38,4 +39,4 @@ print("temps moy2",tmps_moy2)
 #print(g)
 #t1=time.perf_counter()
 #print("temps",t1-to)
-create_output_file("input/network.2.in","input/routes.2.in")
+#create_output_file("input/network.2.in","input/routes.2.in")
